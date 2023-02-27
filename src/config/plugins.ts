f89bd8fd5582461de12645d0e63d9bc879c89c59
Lexus Drumgold <unicornware@flexdevelopment.llc>
2023-02-27 03:29:22 -0500

    feat(config): `plugins`
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

---
+++ src/config/plugins.ts
@@ -0,0 +1,17 @@
+/**
+ * @file Config - plugins
+ * @module commitlint-config/config/plugins
+ */
+
+import type * as commitlint from '@commitlint/types'
+
+/**
+ * Plugins to apply.
+ *
+ * @see https://commitlint.js.org/#/reference-plugins
+ *
+ * @const {(commitlint.Plugin | string)[]} plugins
+ */
+const plugins: (commitlint.Plugin | string)[] = []
+
+export default plugins
